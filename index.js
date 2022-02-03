20412585e0c3e26e70fc51f4e4fd6a8bba864e79
Leonardo <mr_kunkel@hotmail.com>
2022-02-03 09:50:04 -0600

    route created

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ const app = express();
 // 2. Middelwares
 require('dotenv').config()
 
+// 3. Rutas
+app.use('/', require('./routes/index.routes'))
+
 app.listen(process.env.PORT, () => console.log(`It's alive`))
